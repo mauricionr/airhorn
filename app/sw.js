52794d427a898d1a7d91b87efca4501d8f10e538
Mauricio N. Reis <mauricioskateboard@gmail.com>
2016-04-28 15:46:02 -0300

    estudandoooo

--- app/sw.js
+++ app/sw.js
@@ -25,24 +25,24 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('airhorner').then(cache => {
       return cache.addAll([
-        `/`,
-        `/index.html?timestamp=${timeStamp}`,
-        `/styles/main.css?timestamp=${timeStamp}`,
-        `/scripts/main.min.js?timestamp=${timeStamp}`,
-        `/sounds/airhorn.mp3?timestamp=${timeStamp}`
-      ])
-      .then(() => self.skipWaiting());
+          `/`,
+          `/index.html?timestamp=${timeStamp}`,
+          `/styles/main.css?timestamp=${timeStamp}`,
+          `/scripts/main.min.js?timestamp=${timeStamp}`,
+          `/sounds/airhorn.mp3?timestamp=${timeStamp}`
+        ])
+        .then(() => self.skipWaiting());
     })
   )
 });
 
-self.addEventListener('activate',  event => {
+self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
+    caches.match(event.request, { ignoreSearch: true }).then(response => {
       return response || fetch(event.request);
     })
   );
